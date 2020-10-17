a3b4202e7963b0f2cdcd3eabf4c9a9dde29abe2f
oshin0703 <so275417500@gmail.com>
2020-10-18 01:59:35 +0900

    third commit

--- user/views.py
+++ user/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomeView(TemplateView):
-    template_name = "user.index.html"
+    template_name = "user/index.html"
