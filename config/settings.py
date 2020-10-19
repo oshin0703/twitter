8b3ee9a5ce07d7ce40c1ec94fb9be2245c6e1636
oshin0703 <so275417500@gmail.com>
2020-10-20 01:57:25 +0900

    deploy

--- config/settings.py
+++ config/settings.py
@@ -13,12 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
